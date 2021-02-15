695ffa187829ff4dbac7bc89bad7c4669a674c53
Maharshi Shah <40323681+maharshi66@y.github.com>
2021-02-15 20:24:28 +0530

    Added Access controle

--- server.js
+++ server.js
@@ -23,11 +23,16 @@ const db = knex({
   }
 });
 
+ app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next()
+  });
+
 app.use(cors());
 console.log(db.select('*').from('users').then(data => console.log));
 //Middleware
 app.use(bodyParser.json());
-
 //ROUTES
 app.get("/", (req, res) => {res.json("Success!")});
 app.post('/signin', (req, res) => {signin.handleSignIn(req, res, db, bcrypt)});
